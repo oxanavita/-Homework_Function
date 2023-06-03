44ff774050a974188eca15c4cf244e8cf15751df
Oksana <oxana.vita91@mail.ru>
2023-06-03 06:15:51 +0300

    Задача 19 hard

---
+++ Homework_task19_hard/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите целое число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isPalindrome = IsPalindrome(number);
+
+        if (isPalindrome)
+            Console.WriteLine("Число является палиндромом.");
+        else
+            Console.WriteLine("Число не является палиндромом.");
+    }
+
+    static bool IsPalindrome(int number)
+    {
+        int originalNumber = number;
+        int reversedNumber = 0;
+
+        while (number > 0)
+        {
+            int remainder = number % 10;
+            reversedNumber = reversedNumber * 10 + remainder;
+            number /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+}
